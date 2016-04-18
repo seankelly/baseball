403c328e55a5de54bfc525c9f411d1d76ee39703
Sean Kelly <code@katron.org>
2016-04-18 07:55:03 -0500

    Move RetrosheetGameLog to retrosheet mod
    Added another struct that is focused on a specific team and two methods
    for that struct to construct it based on whether it should be from the
    home or visitor team.

--- src/main.rs
+++ src/main.rs
@@ -8,187 +8,7 @@ use std::cmp::Ordering;
 
 use csv::Reader;
 
-#[derive(Clone, Debug, RustcDecodable)]
-struct RetrosheetGameLog {
-    // 1
-    date: String,
-    number_of_game: String,
-    day_of_week: String,
-    visitor_team: String,
-    visitor_league: String,
-    visitor_team_game_number: u16,
-    home_team: String,
-    home_league: String,
-    home_team_game_number: u16,
-    // 10
-    visitor_score: u16,
-    home_score: u16,
-    number_of_outs: Option<u8>,
-    day_night: String,
-    completion_info: String,
-    forfeit_info: String,
-    protest_info: String,
-    park_id: String,
-    attendance: Option<u32>,
-    time_of_game: Option<u16>,
-    // 20
-    visitor_line_score: String,
-    home_line_score: String,
-    visitor_ab: Option<u8>,
-    visitor_hits: Option<u8>,
-    visitor_doubles: Option<u8>,
-    visitor_triples: Option<u8>,
-    visitor_homeruns: Option<u8>,
-    visitor_rbi: Option<u8>,
-    visitor_sac_hits: Option<u8>,
-    visitor_sac_flies: Option<u8>,
-    // 30
-    visitor_hbp: Option<u8>,
-    visitor_walks: Option<u8>,
-    visitor_intentional_walks: Option<u8>,
-    visitor_strikeouts: Option<u8>,
-    visitor_stolen_bases: Option<u8>,
-    visitor_caught_stealing: Option<u8>,
-    visitor_gidp: Option<u8>,
-    visitor_catcher_interference: Option<u8>,
-    visitor_left_on_base: Option<u8>,
-    visitor_pitchers_used: Option<u8>,
-    // 40
-    visitor_individual_earned_runs: Option<u8>,
-    visitor_team_earned_runs: Option<u8>,
-    visitor_wild_pitches: Option<u8>,
-    visitor_balks: Option<u8>,
-    visitor_putouts: Option<u8>,
-    visitor_assists: Option<u8>,
-    visitor_errors: Option<u8>,
-    visitor_passed_balls: Option<u8>,
-    visitor_double_plays: Option<u8>,
-    visitor_triple_plays: Option<u8>,
-    // 50
-    home_ab: Option<u8>,
-    home_hits: Option<u8>,
-    home_doubles: Option<u8>,
-    home_triples: Option<u8>,
-    home_homeruns: Option<u8>,
-    home_rbi: Option<u8>,
-    home_sac_hits: Option<u8>,
-    home_sac_flies: Option<u8>,
-    home_hbp: Option<u8>,
-    home_walks: Option<u8>,
-    // 60
-    home_intentional_walks: Option<u8>,
-    home_strikeouts: Option<u8>,
-    home_stolen_bases: Option<u8>,
-    home_caught_stealing: Option<u8>,
-    home_gidp: Option<u8>,
-    home_catcher_interference: Option<u8>,
-    home_left_on_base: Option<u8>,
-    home_pitchers_used: Option<u8>,
-    home_individual_earned_runs: Option<u8>,
-    home_team_earned_runs: Option<u8>,
-    // 70
-    home_wild_pitches: Option<u8>,
-    home_balks: Option<u8>,
-    home_putouts: Option<u8>,
-    home_assists: Option<u8>,
-    home_errors: Option<u8>,
-    home_passed_balls: Option<u8>,
-    home_double_plays: Option<u8>,
-    home_triple_plays: Option<u8>,
-    home_plate_umpire_name: String,
-    home_plate_umpire_id: String,
-    // 80
-    first_base_umpire_name: String,
-    first_base_umpire_id: String,
-    second_base_umpire_name: String,
-    second_base_umpire_id: String,
-    third_base_umpire_name: String,
-    third_base_umpire_id: String,
-    left_field_umpire_name: String,
-    left_field_umpire_id: String,
-    right_field_umpire_name: String,
-    right_field_umpire_id: String,
-    // 90
-    visitor_manager_name: String,
-    visitor_manager_id: String,
-    home_manager_name: String,
-    home_manager_id: String,
-    winning_pitcher_name: String,
-    winning_pitcher_id: String,
-    losing_pitcher_name: String,
-    losing_pitcher_id: String,
-    saving_pitcher_name: String,
-    saving_pitcher_id: String,
-    // 100
-    gwrbi_player_name: String,
-    gwrbi_player_id: String,
-    visitor_starter_name: String,
-    visitor_starter_id: String,
-    home_starter_name: String,
-    home_starter_id: String,
-    visitor_1_id: String,
-    visitor_1_name: String,
-    visitor_1_pos: String,
-    visitor_2_id: String,
-    // 110
-    visitor_2_name: String,
-    visitor_2_pos: String,
-    visitor_3_id: String,
-    visitor_3_name: String,
-    visitor_3_pos: String,
-    visitor_4_id: String,
-    visitor_4_name: String,
-    visitor_4_pos: String,
-    visitor_5_id: String,
-    visitor_5_name: String,
-    // 120
-    visitor_5_pos: String,
-    visitor_6_id: String,
-    visitor_6_name: String,
-    visitor_6_pos: String,
-    visitor_7_id: String,
-    visitor_7_name: String,
-    visitor_7_pos: String,
-    visitor_8_id: String,
-    visitor_8_name: String,
-    visitor_8_pos: String,
-    // 130
-    visitor_9_id: String,
-    visitor_9_name: String,
-    visitor_9_pos: String,
-    home_1_id: String,
-    home_1_name: String,
-    home_1_pos: String,
-    home_2_id: String,
-    home_2_name: String,
-    home_2_pos: String,
-    home_3_id: String,
-    // 140
-    home_3_name: String,
-    home_3_pos: String,
-    home_4_id: String,
-    home_4_name: String,
-    home_4_pos: String,
-    home_5_id: String,
-    home_5_name: String,
-    home_5_pos: String,
-    home_6_id: String,
-    home_6_name: String,
-    // 150
-    home_6_pos: String,
-    home_7_id: String,
-    home_7_name: String,
-    home_7_pos: String,
-    home_8_id: String,
-    home_8_name: String,
-    home_8_pos: String,
-    home_9_id: String,
-    home_9_name: String,
-    home_9_pos: String,
-    // 160
-    additional_info: String,
-    acquistion_info: String,
-}
+mod retrosheet;
 
 struct Streak {
     team_id: String,
@@ -211,7 +31,7 @@ enum StreakType {
 }
 
 impl Streak {
-    fn from_game(game: &RetrosheetGameLog, home_team: bool) -> Streak {
+    fn from_game(game: &retrosheet::RetrosheetGameLog, home_team: bool) -> Streak {
         let team_id = if home_team {
             game.home_team.clone()
         }
@@ -252,15 +72,15 @@ impl Streak {
     }
 }
 
-fn season_games(file: &str) -> Vec<RetrosheetGameLog> {
+fn season_games(file: &str) -> Vec<retrosheet::RetrosheetGameLog> {
     let mut csv_reader = Reader::from_file(file)
                             .expect("Couldn't open file.")
                             .has_headers(false);
-    let games = csv_reader.decode().collect::<csv::Result<Vec<RetrosheetGameLog>>>().unwrap();
+    let games = csv_reader.decode().collect::<csv::Result<Vec<retrosheet::RetrosheetGameLog>>>().unwrap();
     return games;
 }
 
-fn order_season(games: Vec<RetrosheetGameLog>) -> BTreeMap<String, Vec<RetrosheetGameLog>> {
+fn order_season(games: Vec<retrosheet::RetrosheetGameLog>) -> BTreeMap<String, Vec<retrosheet::RetrosheetGameLog>> {
     let mut season = BTreeMap::new();
 
     for game in games {
@@ -301,7 +121,7 @@ fn order_season(games: Vec<RetrosheetGameLog>) -> BTreeMap<String, Vec<Retroshee
     return season;
 }
 
-fn process_season_streaks(season: BTreeMap<String, Vec<RetrosheetGameLog>>) -> Vec<Streak> {
+fn process_season_streaks(season: BTreeMap<String, Vec<retrosheet::RetrosheetGameLog>>) -> Vec<Streak> {
     let streaks = Vec::new();
     return streaks;
 }
