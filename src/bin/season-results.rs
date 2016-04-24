04e983acab2941142e68d8fd30cd65077e200fe7
Sean Kelly <code@katron.org>
2016-04-23 22:34:44 -0500

    Combine season results with streak data and dump results

--- src/bin/season-results.rs
+++ src/bin/season-results.rs
@@ -20,6 +20,7 @@ struct Streak {
     length: u8,
 }
 
+#[derive(Debug, RustcDecodable, RustcEncodable)]
 struct FullStreak {
     team_id: String,
     year: u16,
@@ -68,6 +69,25 @@ enum StreakType {
 }
 
 
+impl FullStreak {
+    fn from_streak(streak: &Streak, season: &SeasonResults) -> Self {
+        FullStreak {
+            team_id: streak.team_id.clone(),
+            year: streak.year,
+            start_date: streak.start_date.clone(),
+            end_date: streak.end_date.clone(),
+            start_game: streak.start_game,
+            end_game: streak.end_game,
+            streak_type: streak.streak_type.clone(),
+            length: streak.length,
+            final_wins: season.wins,
+            final_losses: season.losses,
+            postseason: season.postseason.clone(),
+        }
+    }
+}
+
+
 fn load_streaks(streaks_file: &str) -> Vec<Streak> {
     let mut csv_reader = Reader::from_file(streaks_file)
                             .expect("Couldn't open file.")
@@ -91,8 +111,28 @@ fn load_results(results_file: &str) -> BTreeMap<(String, u16), SeasonResults> {
     return results_map;
 }
 
-fn join_streaks_with_results(streaks: &Vec<Streak>) -> Vec<FullStreak> {
-    return Vec::new();
+fn join_streaks_with_results(streaks: &Vec<Streak>,
+                             results: &BTreeMap<(String, u16), SeasonResults>)
+                            -> Vec<FullStreak> {
+    let mut full_streaks = Vec::new();
+
+    for streak in streaks {
+        let key = (streak.team_id.clone(), streak.year);
+        let maybe_season = results.get(&key);
+        if let Some(season) = maybe_season {
+            full_streaks.push(FullStreak::from_streak(&streak, &season));
+        }
+    }
+
+    return full_streaks;
+}
+
+fn dump_full_streaks(full_streaks: &Vec<FullStreak>) {
+    let mut writer = Writer::from_memory();
+    for record in full_streaks.into_iter() {
+        let result = writer.encode(record).expect("Encoded streak into CSV.");
+    }
+    print!("{}", writer.as_string());
 }
 
 fn main() {
@@ -106,4 +146,7 @@ fn main() {
     let streaks = load_streaks(&args[1]);
     println!("Loading season results.");
     let results = load_results(&args[2]);
+    println!("Joining streaks with season results.");
+    let full_streaks = join_streaks_with_results(&streaks, &results);
+    dump_full_streaks(&full_streaks);
 }
