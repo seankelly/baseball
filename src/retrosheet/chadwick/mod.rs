117d63bafd2cc918097e694c8dc0f92876de866c
Sean Kelly <code@katron.org>
2019-04-26 16:46:54 -0400

    Add generic function to load Chadwick files

--- src/retrosheet/chadwick/mod.rs
+++ src/retrosheet/chadwick/mod.rs
@@ -1,5 +1,26 @@
+use std::path::Path;
+
+use csv::ReaderBuilder;
+use serde::Deserialize;
+
 
 pub mod games;
 
 pub use games::GameLog;
 pub use games::TeamGameLog;
+
+
+pub fn load_file<T>(file: &Path) -> Vec<T>
+    where for<'de> T: Deserialize<'de>
+    {
+    let mut csv_reader = ReaderBuilder::new()
+                            .has_headers(false)
+                            .from_path(file)
+                            .expect("Couldn't open file.");
+    let mut records = Vec::new();
+    for record in csv_reader.deserialize() {
+        let record: T = record.expect("Couldn't decode record");
+        records.push(record);
+    }
+    return records;
+}
